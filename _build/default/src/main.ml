8c6bf58f51ddf2f4414dc0d01bef4d7d2e358bc9
yvan-parent <yvan.parent77@gmail.com>
2025-03-04 16:53:31 +0100

    removed useless files

--- _build/default/src/main.ml
+++
@@ -1,5 +0,0 @@
-let () =
-  Printf.printf "Enter a file name: \n%!";
-  let input = read_line () in
-  let (n, b, tab, amount_factors, diff_factor_list, hash, rows) = Utilities.read_file_T ("src/test/"^input^".txt") in
-  Utilities.resolution_from_file_T n b tab amount_factors diff_factor_list hash rows (Some ())
